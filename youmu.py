ebe737f8c463a03dc3b8f01333a8adaa01dca7f6
Walter Wu <walterwuyan@gmail.com>
2017-11-14 16:35:25 +0000

    Very first draft of youmu
    A bit stuck with message editting now

---
+++ youmu.py
@@ -0,0 +1,45 @@
+import discord
+from discord.ext import commands
+import asyncio
+
+youmu = discord.ext.commands.Bot(command_prefix="!?",
+                                 description="Discord bot for converting Japanese hiragana and katakana to romaji.",
+                                 pm_help=True)
+japDic = {
+    "ぁ": "a", "あ": "a", "ぃ": "i", "い": "i", "ぅ": "u", "う": "u", "ぇ": "e", "え": "e", "ぉ": "o", "お": "o", "か": "ka",
+    "が": "ga", "き": "ki", "ぎ": "gi", "く": "ku"
+    # ぐ	け	げ	こ	ご	さ	ざ	し	じ	す	ず	せ	ぜ	そ	ぞ	た
+    # だ	ち	ぢ	っ	つ	づ	て	で	と	ど	な	に	ぬ	ね	の	は
+    # ば	ぱ	ひ	び	ぴ	ふ	ぶ	ぷ	へ	べ	ぺ	ほ	ぼ	ぽ	ま	み
+    # む	め	も	ゃ	や	ゅ	ゆ	ょ	よ	ら	り	る	れ	ろ	ゎ	わ
+    # ゐ	ゑ	を	ん	ゔ	ゕ	ゖ
+}
+
+
+@youmu.event
+async def on_ready():
+    print('Logged in as')
+    print(youmu.user.name)
+    print(youmu.user.id)
+    print('------')
+
+
+@youmu.command()
+async def test(message):
+    tmp = await message.channel.send('Calculating messages...')
+    counter = 0
+    async for log in message.channel.history():
+        if await if_jap(log):
+            counter += 1
+
+    await tmp.edit()
+
+
+async def if_jap(message):
+    for char in message.content:
+        if char in japDic:
+            return True
+    return False
+
+
+youmu.run('Mzc5MTkyMzU1MjU0ODI5MDU3.DOmeAQ.rCdugBhUDQSpGfnrtHGtk6OjBw0')
