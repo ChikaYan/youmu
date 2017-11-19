97e73a4f2b91db24cf8084535638fb1bacad09bf
Walter Wu <walterwuyan@gmail.com>
2017-11-19 08:53:37 +0000

    Add config folder and files

--- youmu.py
+++ youmu.py
@@ -1,20 +1,13 @@
+#!/usr/bin/env python
+
 import discord
 from discord.ext import commands
 import asyncio
+from config.keyconfig import key
 
 youmu = discord.ext.commands.Bot(command_prefix="!?",
                                  description="Discord bot to deal with touhou stuff",
                                  pm_help=True)
-japDic = {
-    "ぁ": "a", "あ": "a", "ぃ": "i", "い": "i", "ぅ": "u", "う": "u", "ぇ": "e", "え": "e", "ぉ": "o", "お": "o", "か": "ka",
-    "が": "ga", "き": "ki", "ぎ": "gi", "く": "ku"
-    # ぐ	け	げ	こ	ご	さ	ざ	し	じ	す	ず	せ	ぜ	そ	ぞ	た
-    # だ	ち	ぢ	っ	つ	づ	て	で	と	ど	な	に	ぬ	ね	の	は
-    # ば	ぱ	ひ	び	ぴ	ふ	ぶ	ぷ	へ	べ	ぺ	ほ	ぼ	ぽ	ま	み
-    # む	め	も	ゃ	や	ゅ	ゆ	ょ	よ	ら	り	る	れ	ろ	ゎ	わ
-    # ゐ	ゑ	を	ん	ゔ	ゕ	ゖ
-}
-
 
 @youmu.event
 async def on_ready():
@@ -42,5 +35,4 @@ async def if_jap(message):
     return False
 
 
-keyConfig = open("key.config", "r")
-youmu.run(keyConfig.readline())
+youmu.run(key)
