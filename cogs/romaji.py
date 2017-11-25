bf656cdd33e74f1a82afdab19efedb3d13baae00
Walter Wu <walterwuyan@gmail.com>
2017-11-25 18:47:20 +0000

    Beautify

--- cogs/romaji.py
+++ cogs/romaji.py
@@ -11,7 +11,8 @@ class Romaji:
     @commands.command()
     async def romaji(self, ctx):
         async for message in ctx.channel.history(limit=300):
-            if any(key in message.content for key in JAPDIC) and message.id not in self._jap_log and not message.author.bot:
+            if any(key in message.content for key in
+                   JAPDIC) and message.id not in self._jap_log and not message.author.bot:
                 # the message contains japanese but is not converted before
                 await ctx.channel.send("{}:\n{}".format(message.content, await self.convert(message.content)))
                 self._jap_log.append(message.id)
