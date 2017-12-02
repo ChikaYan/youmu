e1dd2476c2215ef096570e4169881f669a0e865e
Walter Wu <walterwuyan@gmail.com>
2017-12-02 20:06:15 +0000

    Reduce the logging level

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ bot = commands.Bot(command_prefix="!?",
                    description="Discord bot to deal with touhou stuff",
                    pm_help=True)
 logger = logging.getLogger("discord")
-logger.setLevel(logging.DEBUG)
+logger.setLevel(logging.WARNING)
 handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
 handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
 logger.addHandler(handler)
