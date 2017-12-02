30afc96720d35d3dc5ba6b4ff3d58dc44cea330a
Walter Wu <walterwuyan@gmail.com>
2017-12-02 20:31:47 +0000

    Add daily touhou function

--- cogs/pixiv.py
+++ cogs/pixiv.py
@@ -2,6 +2,7 @@ from pixivpy3 import *
 from config.pixivconfig import userid, pw
 import discord
 from discord.ext import commands
+from time import gmtime, strftime
 import os
 
 
@@ -11,23 +12,34 @@ class Pixiv:
         self._api = AppPixivAPI()
         self._api.login(userid, pw)
         self._search_depth = 50000
-        self._mark_min = 100
+        self.bot.loop.create_task(self.daily_touhou())
+
+    async def daily_touhou(self):
+        await self.bot.wait_until_ready()
+        channel = self.bot.get_channel(383359073229209611)
+        while not self.bot.is_closed():
+            if strftime("%H:%M", gmtime()) == "7:30":
+                await self.send_pic(channel, "東方", 1000)
 
     @commands.command()
     async def gimme2hu(self, ctx):
-        json_result = self._api.search_illust("東方", req_auth=True)
+        self.send_pic(ctx.channel, "東方", 100)
+
+    async def send_pic(self, channel, tag, mark_min):
+        json_result = self._api.search_illust(tag, req_auth=True)
         for i in range(self._search_depth):
             for illust in json_result.illusts:
-                if illust.total_bookmarks >= self._mark_min and not illust.is_bookmarked:
+                if illust.total_bookmarks >= mark_min and not illust.is_bookmarked:
                     # the pic is popular and has not been sent before
-                    self._api.download(illust.image_urls.large, name="touhou.jpg", path="./img", )
-                    await ctx.channel.send(file=discord.File("./img/touhou.jpg"))
+                    self._api.download(illust.image_urls.large, name="temp.jpg", path="./img", )
+                    await channel.send(file=discord.File("./img/temp.jpg"))
                     self._api.illust_bookmark_add(illust.id)
-                    os.remove("./img/touhou.jpg")
+                    os.remove("./img/temp.jpg")
                     return
             next_page = self._api.parse_qs(json_result.next_url)
             json_result = self._api.search_illust(**next_page)
 
+        # search depth reached -- need to clean bookmarks
 
 def setup(bot):
     bot.add_cog(Pixiv(bot))
