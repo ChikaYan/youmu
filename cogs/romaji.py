beaec7733eb8f00e7e0bf26ce7b9b6c34dd076d3
Walter Wu <walterwuyan@gmail.com>
2017-12-26 09:40:45 +0000

    Add clean log function

--- cogs/romaji.py
+++ cogs/romaji.py
@@ -6,16 +6,17 @@ from cogs.japdic import JAPDIC
 class Romaji:
     def __init__(self, bot):
         self.bot = bot
-        self._jap_log = []
+        self.jap_log = []
 
     @commands.command()
     async def romaji(self, ctx):
         async for message in ctx.channel.history(limit=50):
             if any(key in message.content for key in
-                   JAPDIC) and message.id not in self._jap_log and not message.author.bot:
-                # the message contains japanese but is not converted before
+                   JAPDIC) and message.id not in self.jap_log and not message.author.bot:
+                # the user-sent message contains japanese but is not converted before
                 await ctx.channel.send("{}:\n{}".format(message.content, await self.convert(message.content)))
-                self._jap_log.append(message.id)
+                self.jap_log.append(message.id)
+                await clean_log()
                 break
 
     async def convert(self, text):
@@ -23,6 +24,10 @@ class Romaji:
             if char in JAPDIC:
                 text = text.replace(char, JAPDIC[char] + " ")
         return text
+
+    async def clean_log(self):
+        if len(self.jap_log) >= 100:
+            self.jap_log = []
 
 
 def setup(bot):
